787e30c9d7015c66870538afe06140ffa7ae98a3
himanshu07Rai <raihimanshu22345@gmail.com>
2024-11-28 23:45:52 +0530

    recursive CTE

---
+++ SQL/recursice_cte.sql
@@ -0,0 +1,11 @@
+WITH RECURSIVE EmployeeHierarchy AS (
+    SELECT id, name, manager_id
+    FROM employees
+    WHERE manager_id IS NULL
+    UNION ALL
+    SELECT e.id, e.name, e.manager_id
+    FROM employees e
+    INNER JOIN EmployeeHierarchy eh
+    ON e.manager_id = eh.id
+)
+SELECT * FROM EmployeeHierarchy;
